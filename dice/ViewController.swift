85b3c6f79e231e4edc511ff75405b0ad0b7bb1d4
zehra gizem duman <zgduman@gmail.com>
2025-02-23 15:58:07 +0300

    dice app oluşturuldu

--- dice/ViewController.swift
+++ dice/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceClick1: UIImageView!
+    @IBOutlet weak var diceClick2: UIImageView!
+    var randomDice1: Int = 0
+    var randomDice2: Int = 0
+    let arrayDice = ["DiceOne","DiceTwo","DiceThree","DiceFour","DiceFive","DiceSix"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func rollButton(_ sender: Any) {
+        randomDice1 = Int.random(in: 0...5)
+        randomDice2 = Int.random(in: 0...5)
+        diceClick1.image = UIImage(named: arrayDice[randomDice1])
+        diceClick2.image = UIImage(named: arrayDice[randomDice2])
+        
+    }
+    
 }
 
